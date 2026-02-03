54a202154df0a9084d89e2472252f047ecb534ae
Digitalandaiacademy <obahjosephchanel@gmail.com>
2026-02-03 03:45:24 +0100

    add pixel facebook

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr" className="overflow-x-hidden">
+      <head>
+        <script dangerouslySetInnerHTML={{ __html: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init', '890189743411990');fbq('track', 'PageView');` }} />
+        <noscript dangerouslySetInnerHTML={{ __html: `<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=890189743411990&ev=PageView&noscript=1" />` }} />
+      </head>
       <body className="antialiased min-h-screen bg-background text-foreground font-sans overflow-x-hidden">
         {children}
         <Analytics />
